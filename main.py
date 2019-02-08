d591b977f33156e5a4d673d8b837e13706854cba
Matthew Pessa <matthew.pessa@gmail.com>
2019-02-08 22:43:09 +0000

    Open input file and exit if no file

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import os
 import sys
 
 def main():
+    input_file = './input.csv'
+
+    try:
+        f = open(input_file, 'r')
+    except:
+        print('The input file does not exist.')
 
 if __name__ == "__main__":
     main()
