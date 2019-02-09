8031228a04f2a83b086071bff7214b70b0246dd8
Matthew Pessa <matthew.pessa@gmail.com>
2019-02-09 00:44:03 +0000

    Fixed typo

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ def main():
 
     # Attempt to open the input file. Exit with error message if it doesn't exist.
     try:
-        f = open(input_file, 'r')
+        with open(input_file, 'r') as f:
+            # Generate the list of words from the given file
+            data = [w for w in f.readline().split(',')]
     except:
         sys.exit('The input file does not exist.')
 
@@ -15,12 +17,6 @@ def main():
     # This isn't really needed in this instance because of running in the docker container
     if os.path.exists('./output.csv'):
         os.remove('./output.csv')
-    
-    # Generate the list of words from the given file
-    data = [w for w in f.readline().split(',')]
-
-    # Close the input file because we no longer need it
-    f.close()
 
     # Sort the strings into descending alphabetical order
     # because that's what the directions said even if the
@@ -29,7 +25,7 @@ def main():
 
     # Open and write the output file. Exit with error message if an error occurs
     try:
-        with open('./output.csv', 'x') as ouput:
+        with open('./output.csv', 'x') as output:
             output.write(','.join(data))
             output.write('\n')
     except:
